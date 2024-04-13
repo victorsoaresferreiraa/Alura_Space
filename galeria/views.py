933435611c42872058b1b0af5facdc19e7fce114
VictorSoaresFerreira <129811759+victorsoaresferreiraa@users.noreply.github.com>
2024-04-13 16:18:06 -0300

    Esta faltando somente o arquivo imagem.html

--- galeria/views.py
+++ galeria/views.py
@@ -4,4 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'galeria/index.html')
 
-
+def imagem(request):
+    return request(request, 'galeria/imagem.html')
