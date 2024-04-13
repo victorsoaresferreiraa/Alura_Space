9612feda9b1dc21605cbe9dc9f439cdb220f4518
VictorSoaresFerreira <129811759+victorsoaresferreiraa@users.noreply.github.com>
2024-04-13 10:37:54 -0300

    cutucando o framework

--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1> Alura Space </h1>')
+    return render(request, 'index.html')
 
 
